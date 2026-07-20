bc4f5a5f779ac3b780a6be000f411fe3ee59b4f6
Ranjeet528 <Ranjeetgurjar528@gmail.com>
2026-07-21 02:52:08 +0530

    fix nodemailer smtp

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,11 +1,17 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
@@ -14,12 +20,10 @@ export const sendEmail = async ({
   subject,
   html,
 }) => {
-
   await transporter.sendMail({
     from: `"Star Palace Hotel" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     html,
   });
-
 };
